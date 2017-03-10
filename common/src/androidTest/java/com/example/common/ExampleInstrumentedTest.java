c86c7b3d847bff92381bb81c31a4b4f88ada6240
CJJ <2425875132@qq.com>
2017-03-10 18:25:56 +0800

    add realm dependencies

--- common/src/androidTest/java/com/example/common/ExampleInstrumentedTest.java
+++ common/src/androidTest/java/com/example/common/ExampleInstrumentedTest.java
@@ -1,4 +1,4 @@
-package com.jiqu.common;
+package com.example.common;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.example.common", appContext.getPackageName());
+        assertEquals("com.example.common.test", appContext.getPackageName());
     }
 }
---
+++ common/src/main/java/com/example/common/util/FormatUtil.java
@@ -0,0 +1,13 @@
+package com.example.common.util;
+
+/**
+ * Created by CJJ on 2017/3/10.
+ *
+ */
+
+public class FormatUtil {
+
+    public static final long day = 24 * 60 * 60;
+    public static final long week = day * 7;
+    public static final long month = week * 30;
+}
